06714f458f6d741778725948ef66938c056a5e5a
Kodufan <kodufan1@gmail.com>
2024-08-15 20:10:05 -0400

    Add plurality detection to `Contribution` string

--- VRCFaceTracking/Models/GithubContributor.cs
+++ VRCFaceTracking/Models/GithubContributor.cs
@@ -5,4 +5,5 @@ public class GithubContributor
     public string login { get; set; }
     public string html_url { get; set; }
     public int contributions { get; set; }
+    public string contribution_string => $" Contribution{(contributions == 1 ? "" : "s")}";
 }
